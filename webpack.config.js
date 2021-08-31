429448a84b91cf760f47594b9d580fdc494a3b64
Hanna Mudrak <annmudrak@gmail.com>
2021-08-31 14:15:01 -0500

    improve file

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,10 @@ module.exports = {
           },
         },
       },
-      { test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
